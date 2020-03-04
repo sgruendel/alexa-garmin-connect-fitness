47c5a19f289a889b0f2357084a71474d6d690adf
Stefan Gründel <stefan.gruendel@garmin.com>
2020-03-04 14:38:08 +0100

    Use specific GC test account.

--- test/unit/garmin-connect.js
+++ test/unit/garmin-connect.js
@@ -6,51 +6,52 @@ const gc = require('../../src/garmin-connect');
 
 describe('Garmin Connect', () => {
 
-    let jar;
+    let jar, userId;
 
     describe('#login()', () => {
-        xit('should fail with wrong locale', async function() {
-            const response = await gc.login('alexa', 'xxx', '');
-            expect(response).to.throw();
-        });
-
-        // don't execute normally , otherwise account gets deactivated
         it('should fail with wrong credentials', async function() {
-            const response = await gc.login('alexa', 'xxx', 'de-DE');
+            const response = await gc.login('alexa_fail', 'xxx', 'de-DE');
             expect(response.error).to.equal('Ungültige Anmeldung. (Bei Kennwörtern ist die Groß- und Kleinschreibung zu beachten.)');
         });
 
         it('should work with correct credentials', async function() {
-            const response = await gc.login('stefan.gruendel', '83oJ1GBIpr*w', 'de-DE');
+            const response = await gc.login('alexa_german@web.de', 'AlexaGerman42', 'de-DE');
             expect(response.error, 'no error').to.equal('');
             expect(response.jar.getCookies('https://garmin.com/'), 'cookies set').to.be.an('array');
             expect(response.userPreferences.displayName, 'userPreferences read').to.be.a('string');
             expect(response.socialProfile.displayName, 'socialProfile has displayName').to.be.a('string');
             expect(response.socialProfile.id, 'socialProfile has userId').to.be.a('number');
             jar = response.jar;
+            userId = response.socialProfile.id;
         });
     });
 
     describe('#dailySummary()', () => {
         it('should work for date', async function() {
-            const response = await gc.wellnessService.dailySummary('7858235', {date: '2020-02-27'}, jar);
-            console.log(response);
-            expect(response.steps).to.be.a('number');
+            const response = await gc.wellnessService.dailySummary(userId, { date: '2020-03-04' }, jar);
+            expect(response.id, 'id').to.equal('5ce44e911a854ee88b31de583e3d77f5');
+            expect(response.startTimestampLocal, 'startTimestampLocal').to.equal('2020-03-04T00:00:00.0');
+            expect(response.endTimestampLocal, 'endTimestampLocal').to.equal('2020-03-04T14:06:00.0');
+            expect(response.steps, 'steps').to.equal(591);
         });
 
         it('should work for id', async function() {
-            const response = await gc.wellnessService.dailySummary('7858235', {uuid: '31329d01b0be48ee8f6d6cd1cee1a7dd'}, jar);
-            console.log(response);
-            expect(response.steps).to.be.a('number');
+            const response = await gc.wellnessService.dailySummary(userId, { uuid: '5ce44e911a854ee88b31de583e3d77f5' }, jar);
+            expect(response.id, 'id').to.equal('5ce44e911a854ee88b31de583e3d77f5');
+            expect(response.startTimestampLocal, 'startTimestampLocal').to.equal('2020-03-04T00:00:00.0');
+            expect(response.endTimestampLocal, 'endTimestampLocal').to.equal('2020-03-04T14:06:00.0');
+            expect(response.steps, 'steps').to.equal(591);
         });
     });
 
     describe('#activities()', () => {
         it('should work', async function() {
-            const response = await gc.activitylistService.activities('7858235', jar, 0, 1);
-            console.log(response);
+            const response = await gc.activitylistService.activities('378206723', jar, 0, 1);
             expect(response.activityList).to.be.an('array');
-            expect(response.activityList[0].activityName).to.be.a('string');
+            expect(response.activityList).have.length(1);
+            expect(response.activityList[0].activityName, 'activityName').to.equal('Würzburg Laufen');
+            expect(response.activityList[0].activityType.typeKey, 'activityType').equal('running');
+            expect(response.activityList[0].eventType.typeKey, 'eventType').equal('training');
         });
     });
 });
